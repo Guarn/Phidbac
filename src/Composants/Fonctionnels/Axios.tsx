37cf7020f66197edc275b99714b78922a00e3a4c
Florian Jung - Fixe <florianjung885@gmail.com>
2019-10-30 09:31:48 +0100

    Passage version responsivve

--- src/Composants/Fonctionnels/Axios.tsx
+++ src/Composants/Fonctionnels/Axios.tsx
@@ -2,7 +2,10 @@ import axios from "axios";
 
 let reg = /token=.*;*/g;
 let test: string = "";
+console.log(reg.test(document.cookie));
+console.log(document.cookie);
 if (reg.test(document.cookie)) {
+    console.log('entré')
     let test = document.cookie.match(reg)![0].substring(6);
     if (test.charAt(test.length - 1) === ";")
         test = test.slice(0, test.length - 1);
